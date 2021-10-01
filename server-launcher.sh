827c75cd4f392e36e571a5214ab51525c3cb8275
clonethinh <87403984+clonethinh@users.noreply.github.com>
2021-10-01 21:28:52 +0700

    Update server-launcher.sh

--- server-launcher.sh
+++ server-launcher.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+#
+# Script to launch Stremio server
+#
+# Written by https://github.com/shivasiddharth
+
+# Start the server
 cd /home/ubuntu/Stremio-RaspberryPi/stremio-shell/
 node ./server.js
